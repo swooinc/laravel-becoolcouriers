e2d40e0f1f00d64dc352e0b620ce3f72f81d3bdd
Willy Southiphonh <willy@swoo.ca>
2024-12-12 08:42:41 -0500

    :hammer: Simplify to getAll() to get()

--- src/Zone.php
+++ src/Zone.php
@@ -45,7 +45,7 @@ class Zone extends Fluent
      *
      * @return \Illuminate\Support\Collection
      */
-    public static function getAll(): Collection
+    public static function get(): Collection
     {
         return ZoneBuilder::make()->get();
     }
